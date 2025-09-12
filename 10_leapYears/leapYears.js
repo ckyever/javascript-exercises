3e7cb73d8b4f16564646a6467e73ddd55e1df653
ckyever <ckyeverg@gmail.com>
2025-09-12 22:58:21 +1000

    Add solution to 10_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isCentury = (year % 100 === 0);
 
+    if (isCentury) {
+        return (year % 400 === 0);
+    } else {
+        return (year % 4 === 0);
+    }
 };
 
 // Do not edit below this line
