7ff5338903db85ab4936948266c955768d251376
ckyever <ckyeverg@gmail.com>
2025-09-12 22:32:46 +1000

    Add solution to 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (!Number.isInteger(x) || !Number.isInteger(y) || x < 0 || y < 0) {
+        return 'ERROR';
+    }
 
+    if (x < y) {
+        [start, end] = [x, y];
+    } else {
+        [start, end] = [y, x];
+    }
+
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
