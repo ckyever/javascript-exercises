3d5cbf9b3239658a502e9946240cf9204f0a249e
ckyever <ckyeverg@gmail.com>
2025-09-15 14:17:52 +1000

    Add solution for 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  if (numbers.length === 0) {
+    return 0;
+  } else {
+    return numbers.reduce((total, number) => total + number);
+  } 
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  if (numbers.length === 0) {
+    return 0;
+  } else {
+    return numbers.reduce((total, number) => total * number);
+  } 
 
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1
+  for (let i = 1; i <= number; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
