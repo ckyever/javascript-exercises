ba3793ddc82459bb56b27e4b2daa25889f829781
ckyever <ckyeverg@gmail.com>
2025-09-15 15:20:23 +1000

    Add solution for 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestAge = 0;
+    let oldestPerson;
 
+    people.forEach((person) =>{
+        const currentYear = new Date().getFullYear();
+        const endDate = person.yearOfDeath ? person.yearOfDeath : currentYear
+        const age = endDate - person.yearOfBirth;
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestPerson = person;
+        }
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
