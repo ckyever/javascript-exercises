358ffd91513eb49c8a3da41a1851413d7affa3d0
ckyever <ckyeverg@gmail.com>
2025-09-15 14:30:55 +1000

    Add solution for 13_palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const clean = text.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    const reverse = clean.split("").reverse().join("");
+    return (clean === reverse);
 };
 
 // Do not edit below this line
