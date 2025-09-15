fa23b732a805fc3ba4685f244b4d5dbe6f3075e9
ckyever <ckyeverg@gmail.com>
2025-09-15 14:50:13 +1000

    Add solution for exercise 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    const number = Number(input);
+    if (number < 0) {
+        return "OOPS";
+    }
 
+    if (number === 0) {
+        return 0;
+    }
+
+    if (number === 1) {
+        return 1;
+    }
+
+    return fibonacci(number-2) + fibonacci(number-1);
 };
 
 // Do not edit below this line
